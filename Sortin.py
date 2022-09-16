6664b13e2b4a697b3f2d467c6f6a894713d6a1e2
KingAndrew801 <111556315+KingAndrew801@users.noreply.github.com>
2022-09-16 16:43:50 -0600

    started working on team divider
    word

--- Sortin.py
+++ Sortin.py
@@ -1,9 +1,13 @@
 import constants
 
 data = constants.PLAYERS.copy()
+Panthers = []
+Bandits = []
+Warriors = []
+teams = Panthers, Bandits, Warriors
 
 if __name__ == '__main__':
-    def cleandata(iter):
+    def clean_data(iter):
             for i in iter:
                 if i['experience'] == 'YES':
                     i['experience']  = True
@@ -12,8 +16,27 @@ if __name__ == '__main__':
 
                 i['height'] = i['height'].split()
                 i['height'] = int(i['height'][0])
-
                 i['guardians'] = i['guardians'].split(' and ')
 
-cleandata(data)
-print(data)
+
+    def balance_teams(iter):
+        exp_players = []
+        newbs = []
+
+        for i in iter:
+            if i['experience'] == True:
+                exp_players.append(i)
+            else:
+                newbs.append(i)
+
+        teamnum = len(teams)
+        xpplayerct = len(exp_players)
+        for i in teams:
+            for n in range(3):
+                for p in exp_players:
+                    i.append(p['name'])
+                    exp_players.remove(p)
+
+newnie = clean_data(data)
+balance_teams(data)
+print (teams)
